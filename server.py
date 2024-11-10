9ca57d9deba4dbb471e6e1729da10a334844dbe1
noobgokf1 <noobgokf1@gmail.com>
2024-11-10 15:47:40 +0100

    Update server.py

--- server.py
+++ server.py
@@ -123,4 +123,4 @@ def add_review():
     return jsonify({"status": "success", "message": "Review added successfully"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, port=5000)
